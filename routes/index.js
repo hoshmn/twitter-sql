d01bca3d062df5cc19206467218eb43da2404ff5
“Joshua <j.maas.howard@gmail.com>
2016-08-05 14:59:50 -0400

    success!!

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,16 @@ module.exports = function makeRouterWithSockets (io) {
 
   // a reusable function
   function respondWithAllTweets (req, res, next){
-    var allTheTweets = tweetBank.list();
-    res.render('index', {
-      title: 'Twitter.js',
-      tweets: allTheTweets,
-      showForm: true
+    client.query('SELECT * FROM tweets INNER JOIN users ON users.id = tweets.userid ', function(err, result){
+      if (err) return next(err);
+      var allTheTweets = result.rows;
+      console.log(Date(), result.rows);
+      res.render('index', {
+        title: 'Twitter.js',
+        tweets: allTheTweets,
+        showForm: true
+      });
+    
     });
   }
 
@@ -21,30 +26,70 @@ module.exports = function makeRouterWithSockets (io) {
   router.get('/tweets', respondWithAllTweets);
 
   // single-user page
-  router.get('/users/:username', function(req, res, next){
-    var tweetsForName = tweetBank.find({ name: req.params.username });
-    res.render('index', {
-      title: 'Twitter.js',
-      tweets: tweetsForName,
-      showForm: true,
-      username: req.params.username
-    });
+  router.get('/users/:username', function(req, res, next) {
+    client.query('SELECT * FROM tweets INNER JOIN users ON users.id = tweets.userid WHERE users.name = $1',[req.params.username], 
+      function(err, result){
+        if (err) return next(err);
+        var tweetsForName = result.rows;
+        res.render('index', {
+          title: 'Twitter.js',
+          tweets: tweetsForName,
+          showForm: true,
+          //username: req.params.username
+        });
+      });
+    
   });
 
   // single-tweet page
   router.get('/tweets/:id', function(req, res, next){
-    var tweetsWithThatId = tweetBank.find({ id: Number(req.params.id) });
-    res.render('index', {
-      title: 'Twitter.js',
-      tweets: tweetsWithThatId // an array of only one element ;-)
+    client.query('SELECT * FROM tweets INNER JOIN users ON users.id = tweets.userid WHERE tweets.id = $1', [req.params.id],
+      function(err, result){
+        if (err) return next(err);
+        var tweetsWithThatId = result.rows;
+        res.render('index', {
+          title: 'Twitter.js',
+          tweets: tweetsWithThatId,
+          showForm: false,
+
+        });
     });
   });
 
   // create a new tweet
   router.post('/tweets', function(req, res, next){
-    var newTweet = tweetBank.add(req.body.name, req.body.content);
-    io.sockets.emit('new_tweet', newTweet);
-    res.redirect('/');
+    client.query('SELECT * FROM tweets INNER JOIN users ON users.id = tweets.userid WHERE users.name = $1', [req.body.name],
+      function(err, result){
+        if (err) return next(err);
+        var firstTweetOfUser = result.rows[0];
+        var userId;
+
+        if (!firstTweetOfUser){
+
+          client.query('INSERT INTO users (name, pictureurl) VALUES ($1, \'http://i.imgur.com/I8WtzSw.jpg\') RETURNING id', [req.body.name],
+            function(err, result){
+              if (err) return next(err);
+              userId = result.rows[0].id;
+              insertTweet(req.body.name, userId, req.body.content);
+            });
+        } else {
+          userId = firstTweetOfUser.id;
+          insertTweet(req.body.name, userId, req.body.content);
+        }
+        //console.log(result.rows, result.rows[0].name);
+        function insertTweet(name, userId, content){
+        client.query('INSERT INTO tweets (userid, content) VALUES ($1,$2) RETURNING id', [userId, content],
+          function(err, result){
+            if (err) return next(err);
+            console.log('insert complete');
+            var nextTweetId =  result.rows[0];
+            var newTweet =  {name: name, content: content, id: nextTweetId };
+            io.sockets.emit('new_tweet', newTweet);
+            res.redirect('/');
+          });
+        }
+
+      });
   });
 
   // // replaced this hard-coded route with general static routing in app.js
@@ -53,4 +98,4 @@ module.exports = function makeRouterWithSockets (io) {
   // });
 
   return router;
-}
+};
--- tweetBank.js
+++ tweetBank.js
@@ -1,38 +1,39 @@
-'use strict';
 
-var _ = require('lodash');
-var data = [];
+// 'use strict';
 
-function add (name, content) {
-  data.push({ name: name, content: content, id: data.length });
-  return _.clone(data[data.length - 1]);
-}
+// var _ = require('lodash');
+// var data = [];
 
-function list () {
-  return _.cloneDeep(data);
-}
+// function add (name, content) {
+//   data.push({ name: name, content: content, id: data.length });
+//   return _.clone(data[data.length - 1]);
+// }
 
-function find (properties) {
-  return _.cloneDeep(_.filter(data, properties));
-}
+// function list () {
+//   return _.cloneDeep(data);
+// }
 
-module.exports = { add: add, list: list, find: find };
+// function find (properties) {
+//   return _.cloneDeep(_.filter(data, properties));
+// }
 
-var randArrayEl = function(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-};
+// module.exports = { add: add, list: list, find: find };
 
-var getFakeName = function() {
-  var fakeFirsts = ['Nimit', 'Dave', 'Shanna', 'Charlotte', 'Scott', 'Ayana', 'Omri', 'Gabriel', 'Joe'];
-  var fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
-  return randArrayEl(fakeFirsts) + ' ' + randArrayEl(fakeLasts);
-};
+// var randArrayEl = function(arr) {
+//   return arr[Math.floor(Math.random() * arr.length)];
+// };
 
-var getFakeTweet = function() {
-  var awesomeAdj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing'];
-  return 'Fullstack Academy is ' + randArrayEl(awesomeAdj) + '! The instructors are just so ' + randArrayEl(awesomeAdj) + '. #fullstacklove #codedreams';
-};
+// var getFakeName = function() {
+//   var fakeFirsts = ['Nimit', 'Dave', 'Shanna', 'Charlotte', 'Scott', 'Ayana', 'Omri', 'Gabriel', 'Joe'];
+//   var fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
+//   return randArrayEl(fakeFirsts) + ' ' + randArrayEl(fakeLasts);
+// };
 
-for (var i = 0; i < 10; i++) {
-  module.exports.add( getFakeName(), getFakeTweet() );
-}
+// var getFakeTweet = function() {
+//   var awesomeAdj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing'];
+//   return 'Fullstack Academy is ' + randArrayEl(awesomeAdj) + '! The instructors are just so ' + randArrayEl(awesomeAdj) + '. #fullstacklove #codedreams';
+// };
+
+// for (var i = 0; i < 10; i++) {
+//   module.exports.add( getFakeName(), getFakeTweet() );
+// }
